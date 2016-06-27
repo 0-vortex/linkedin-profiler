d4e4d0d0867c66456f0ecefce6f6f54c55cf3994
TED Vortex <ted.vortex@gmail.com>
2016-06-27 06:28:22 +0100

    Coveralls test

---
+++ cli/lin.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+/**
+ * Module dependencies.
+ */
+
+var program = require('commander');
+
+program
+    .version('0.1.0')
+    .command('search [profile]', 'search for a linkedin profile and get available data');
+
+program.parse(process.argv);
